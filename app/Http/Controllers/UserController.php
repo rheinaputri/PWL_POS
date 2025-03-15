0203442c35f458f756aeaa6cfa0069fe7ef55f51
Rheina Putri <rheinaputri8@gmail.com>
2025-03-15 11:55:09 +0700

    Jobsheet 4 - Praktikum 2.6 - CRUD

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,27 +46,79 @@ class UserController extends Controller
 {
     public function index()
     {
-        // Membuat instance model UserModel baru dan menyimpannya ke database
-        $user = UserModel::create([
-            'username' => 'manager11',
-            'nama' => 'Manager 11',
-            'password' => Hash::make('12345'), // Enkripsi password
-            'level_id' => 2,
+        // prak 2.5
+        // // Membuat instance model UserModel baru dan menyimpannya ke database
+        // $user = UserModel::create([
+        //     'username' => 'manager11',
+        //     'nama' => 'Manager 11',
+        //     'password' => Hash::make('12345'), // Enkripsi password
+        //     'level_id' => 2,
+        // ]);
+
+        // // Mengubah nilai atribut username pada instance model
+        // $user->username = 'manager12';
+
+        // // Menyimpan perubahan ke database
+        // $user->save();
+
+        // // Memeriksa apakah ada perubahan pada model setelah disimpan
+        // $user->wasChanged(); // true
+        // $user->wasChanged('username'); // true
+        // $user->wasChanged(['username', 'level_id']); // true
+        // $user->wasChanged('nama'); // false
+
+        // // Menampilkan hasil wasChanged()
+        // dd($user->wasChanged(['nama', 'username'])); // true
+
+        // prak 2.6
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make('$request->password'),
+            'level_id' => $request->level_id
         ]);
 
-        // Mengubah nilai atribut username pada instance model
-        $user->username = 'manager12';
+        return redirect('/user');
+    }
+
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request)
+    {
+        // Mencari user berdasarkan ID
+        $user = UserModel::find($id);
+
+        // Memperbarui atribut user dengan data dari request
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make('$request->password');
+        $user->level_id = $request->level_id;
 
         // Menyimpan perubahan ke database
         $user->save();
 
-        // Memeriksa apakah ada perubahan pada model setelah disimpan
-        $user->wasChanged(); // true
-        $user->wasChanged('username'); // true
-        $user->wasChanged(['username', 'level_id']); // true
-        $user->wasChanged('nama'); // false
+        return redirect('/user');
+    }
 
-        // Menampilkan hasil wasChanged()
-        dd($user->wasChanged(['nama', 'username'])); // true
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+        return redirect('/user');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ Route::get('/kategori', [KategoriController::class, 'index']);
 
 // Route untuk User
 Route::get('/user', [UserController::class, 'index']);
+
+// Route tambah data
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+
+// Route tambah_simpan
+Route::post('user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+
+// Route ubah
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+
+// Route ubah_simpan
+Route::put('user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+
+// Route delete
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
