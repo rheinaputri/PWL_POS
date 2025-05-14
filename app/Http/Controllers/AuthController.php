fbbdbd2769e2362fb25bf780cf5a2c9e3df7bc72
Rheina Putri <rheinaputri8@gmail.com>
2025-05-14 21:40:33 +0700

    Jobsheet 7 PWL POS - Authentication - Tugas 4 Register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,54 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\UserModel;
+use App\Models\LevelModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+
+    public function register()
+    {
+        $levels = LevelModel::select('level_id', 'level_nama')->get();
+
+        return view('auth.register')->with('levels', $levels);
+    }
+
+    public function postRegister(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string|unique:m_user,username',
+            'nama' => 'required|string|max:255',
+            'password' => 'required|string|min:5|confirmed',
+            'level_id' => 'required|exists:m_level,level_id',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation Failed.',
+                'errors' => $validator->errors(),
+            ]);
+        }
+
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => bcrypt($request->password),
+            'level_id' => $request->level_id
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Registration Success.',
+            'redirect' => url('/login')
+        ]);
+    }
+
     public function login()
     {
         if (Auth::check()) { // jika sudah login, maka redirect ke halaman home
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::pattern('id', '[0-9]+'); // artinya ketika ada parameter {id}, maka harus
 
 Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'postlogin']);
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/register', [AuthController::class, 'postRegister']);
 Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
 
 Route::middleware(['auth'])->group(function () { // artinya semua route di dalam group ini harus login dulu
